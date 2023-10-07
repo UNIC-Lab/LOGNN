18cf54b83fd226b42c2d852e05648a489f20059d
LionelFu <90789521+LionelFu@users.noreply.github.com>
2023-10-07 11:27:48 +0800

    Update critic_train_exp.py

--- critic_train_exp.py
+++ critic_train_exp.py
@@ -2,114 +2,165 @@
 import numpy as np
 import torch
 import torch.nn as nn
-from tqdm import tqdm
-import pandas as pd
 import matplotlib.pyplot as plt
-from torch_geometric.utils import softmax
 from off_loading_models import PCNet, PCNetCritic, MMSE, GnnCritic, TaskLoad
-from tqdm import tqdm
 from arguments import args
 from torch_geometric.loader import DataLoader
 from layouts import generate_layouts
-import pandas as pd
 from tqdm import tqdm
-from time import time
 
 
-def compute_loss(task_allocation, power_allocation, comp_allocation, compute_resource, path_losses, task_size, edge_index):
+def compute_loss_nn(task_allocation, power_allocation, comp_allocation, task_size, compute_resource, path_losses, user_index, server_index):
+    
+    # task_size : vector N
+    # task_allocation: mat pop_num x 3*M*N
+    # index: vector 3*M*N
+    
     epsilon = 1e-9
     extre = 1e-20
-    user_index = edge_index[0]      # s2u中源节点的索引
-    server_index = edge_index[1]    # s2u中目标节点的索引
+    server_index_first = server_index.reshape((batch_size, -1))[0]
+    user_index_first = user_index.reshape((batch_size, -1))[0]
+    # user_index = edge_index[0]      # s2u中源节点的索引
+    # server_index = edge_index[1]    # s2u中目标节点的索引
     
-    task_size = task_size[user_index]
-    # task_size = task_size[user_index]*args.tasksize_cof       # 重复采样映射到边中
-    tasks = task_size * task_allocation.squeeze()
-    compute_resource = compute_resource[server_index]
-    # compute_resource = compute_resource[server_index]*args.comp_cof       # 
-    comp = compute_resource * comp_allocation.squeeze()
-    pw = power_allocation.squeeze() * path_losses.squeeze()
-
+    # power_allocation = torch.clamp(power_allocation, 1e-5, 1)
+    pw_ini = power_allocation * path_losses    # mat pop_num x M*N
     
-    # pws_list = []
-    # for idx in range(server_index[-1]+1):
-    #     # 取出所有到同一个server的pw加和
-    #     pws_idx = pw[torch.where(server_index==idx)].sum()
-    #     pws_list.append(pws_idx)
-    # pws_list = torch.stack(pws_list)
-    # pws_list = pws_list[server_index]
+    # 将信道状态过小的设置为0
+    mask_pw = torch.where(pw_ini<args.pw_threshold)
+    
+    pw = pw_ini.clone()
 
-    pw_list = torch.zeros((len(pw), server_index[-1]+1), device=args.device)
-    pw_list.scatter_(1, server_index.unsqueeze(1), pw.unsqueeze(1))
-    pws_list = pw_list.sum(0)[server_index]
+    pw[mask_pw] = 0
+
+    comp_allocation_clone = comp_allocation.clone()
+    comp_allocation_clone[mask_pw] = 0
+    comp_allocation_normed = torch.zeros((comp_allocation_clone.shape[0], comp_allocation_clone.shape[1], server_index_first[-1]+1), device=args.device)
+    comp_allocation_normed.scatter_(2, server_index_first.repeat((batch_size, 1)).unsqueeze(2), comp_allocation_clone.unsqueeze(2))
+    comp_allocation_normed = comp_allocation_normed.sum(1)[:, server_index_first]
+    comp_allocation_normed = torch.div(comp_allocation_clone, comp_allocation_normed+extre)
+
+    task_allocation_clone = task_allocation.clone()
+    task_allocation_clone[mask_pw] = 0
+    task_allocation_normed = torch.zeros((task_allocation_clone.shape[0], task_allocation_clone.shape[1], user_index_first[-1]+1), device=args.device)
+    task_allocation_normed.scatter_(2, user_index_first.repeat((batch_size, 1)).unsqueeze(2), task_allocation_clone.unsqueeze(2))
+    task_allocation_normed = task_allocation_normed.sum(1)[:, user_index_first]
+    task_allocation_normed = torch.div(task_allocation_clone, task_allocation_normed+extre)
+
+    # 计算速率
+    pw_list = torch.zeros((pw.shape[0], pw.shape[1], server_index_first[-1]+1), device=args.device)   # mat pop_num x MN x N
+    pw_list.scatter_(2, server_index_first.repeat((batch_size, 1)).unsqueeze(2), pw.unsqueeze(2))
+    pws_list = pw_list.sum(1)[:, server_index_first]  # mat pop_num x MN
 
     interference = pws_list-pw
     rate = torch.log2(1+torch.div(pw, interference+epsilon))
     # rate = args.band_width * torch.log2(1+torch.div(pw, interference+epsilon))
+
+
+    task_size = task_size[:, user_index_first]       # M*N    
+    # task_size = task_size[user_index]*args.tasksize_cof       # 重复采样映射到边中
+    tasks = task_size * task_allocation_normed   # mat pop_num x M*N
+
+    compute_resource = compute_resource[:, server_index_first]
+    # compute_resource = compute_resource[server_index]*args.comp_cof       # 
+
+    comp = compute_resource * comp_allocation_normed
+
+
     # offloading_time = torch.div(tasks, rate+extre) * (args.tasksize_cof/args.band_width)
     offloading_time = torch.div(tasks, rate+extre)
 
     # compute_time = torch.div(tasks, comp+extre) * (args.tasksize_cof*args.cons_factor/args.comp_cof)
     compute_time = torch.div(tasks, comp+extre)
 
-    # compute_time = torch.clamp(compute_time, -1, 3000)
-    # offloading_time = torch.clamp(offloading_time, -1, 3000)
-
-    time_loss = offloading_time + compute_time
+    time_loss = offloading_time + compute_time      # pop_num x MN
     assert torch.isnan(time_loss).sum()==0
 
-    # time_loss_list = []
-    time_loss_list = torch.zeros((len(time_loss), user_index[-1]+1), device=args.device)
-    time_loss_list.scatter_(1, user_index.unsqueeze(1), time_loss.unsqueeze(1))
-    time_loss_list = time_loss_list.sum(0)
 
-    # for idx in range(user_index[-1]+1):
-    #     tl_idx = time_loss[torch.where(user_index==idx)].sum()
-    #     time_loss_list.append(tl_idx)
-    # time_loss_list = torch.stack(time_loss_list)
+    time_loss_list = torch.zeros((time_loss.shape[0], time_loss.shape[1], user_index_first[-1]+1), device=args.device)
+    time_loss_list.scatter_(2, user_index_first.repeat((batch_size, 1)).unsqueeze(2), time_loss.unsqueeze(2))
+    time_loss_list = time_loss_list.sum(1)      # pop_num x MN
 
     return time_loss_list.mean()
 
 
-
-def compute_loss_nn(server_num, task_allocation, power_allocation, comp_allocation, compute_resource, path_losses, task_size):
+def compute_loss(task_allocation, power_allocation, comp_allocation, compute_resource, path_losses, task_size, user_index, server_index):
     
-    '''
-        allocation: batch_size x server_num*user_num
-        path_losses: batch_size x server_num*user_num
-    '''
-
     epsilon = 1e-9
     extre = 1e-20
+    # user_index = edge_index[0]      # s2u中源节点的索引
+    # server_index = edge_index[1]    # s2u中目标节点的索引
+    
+    pw_ini = power_allocation.squeeze() * path_losses.squeeze()
+    # pw小于阈值的对应power设为0
+    pw = pw_ini.clone()
+    # mask_pw = torch.where(pw_ini<args.pw_threshold)
+    # pw[mask_pw] = 0
 
-    batch_size = task_allocation.shape[0]
-    task_allocation = task_allocation.reshape((batch_size, -1, server_num))
-    power_allocation = power_allocation.reshape((batch_size, -1, server_num))
-    comp_allocation = comp_allocation.reshape((batch_size, -1, server_num))
-    # compute_resource = compute_resource.reshape((batch_size, -1, server_num))
-    path_losses = path_losses.reshape((batch_size, -1, server_num))
-    task_size = task_size.unsqueeze(-1).repeat((1, 1, server_num))
-    compute_resource = compute_resource.unsqueeze(1).repeat((1, 3*server_num, 1))
+    pw_user_list = torch.zeros((len(pw), user_index[-1]+1), device=args.device)
+    # pw_user_ini_list = torch.zeros((len(pw), user_index[-1]+1), device=args.device)
+    # pw_user_ini_list.scatter_(1, user_index.unsqueeze(1), pw_ini.unsqueeze(1))
+    pw_user_list.scatter_(1, user_index.unsqueeze(1), pw_ini.unsqueeze(1))
+    # 如果某一个user的发射功率均位于阈值以下
+    pw_masked = pw_user_list.clone()
+    pw_masked[torch.where(pw_masked < args.pw_threshold)] = 0
+    invalid_index = torch.where(pw_masked.sum(0)==0)[0]   # 是否有对所有server都低于阈值的
+    # assert len(invalid_index)==0
+    max_pw_index = pw_user_list[:, invalid_index].argmax(0) # 对所有server的pw都低于阈值的user 取信号最强的server
+    pw_masked[max_pw_index, invalid_index] = pw_user_list[max_pw_index, invalid_index]
+    pw = pw_masked.sum(1)
+    mask_pw = torch.where(pw==0)
+
+    pw_list = torch.zeros((len(pw), server_index[-1]+1), device=args.device)
+    pw_list.scatter_(1, server_index.unsqueeze(1), pw.unsqueeze(1))
+    
+    pws_list = pw_list.sum(0)[server_index]
 
 
-    pw = torch.mul(power_allocation, path_losses)
-    pw_server = pw.sum(1).unsqueeze(1).repeat((1, 3*server_num, 1))
-    interference = pw_server-pw
+    interference = pws_list-pw
     rate = torch.log2(1+torch.div(pw, interference+epsilon))
 
-    tasks = torch.mul(task_size, task_allocation)
+    task_allocation_clone = task_allocation.clone().squeeze() + 1e-8
+    task_allocation_clone[mask_pw] = 0
+    task_allocation_normed = torch.zeros((len(task_allocation_clone), user_index[-1]+1), device=args.device)
+    task_allocation_normed.scatter_(1, user_index.unsqueeze(1), task_allocation_clone.unsqueeze(1))
+    # assert len(torch.where(task_allocation_normed.sum(0)==0)[0]) == 0
+    task_allocation_normed_2 = task_allocation_normed.sum(0)[user_index]
+    task_allocation_final = torch.div(task_allocation_clone, task_allocation_normed_2+extre)
+    # task_allocation_clone =  softmax(task_allocation_clone, user_index)
+
+    task_size = task_size[user_index]
+    # task_size = task_size[user_index]*args.tasksize_cof       # 重复采样映射到边中
+    
+    tasks = task_size * task_allocation_final
+
+    comp_allocation_clone = comp_allocation.clone().squeeze()
+    comp_allocation_clone[mask_pw] = 0
+    comp_allocation_normed = torch.zeros((len(comp_allocation_clone), server_index[-1]+1), device=args.device)
+    comp_allocation_normed.scatter_(1, server_index.unsqueeze(1), comp_allocation_clone.unsqueeze(1))
+    comp_allocation_normed_2 = comp_allocation_normed.sum(0)[server_index]
+    comp_allocation_final = torch.div(comp_allocation_clone, comp_allocation_normed_2+extre)
+    # compute_resource = compute_resource[server_index]*args.comp_cof       # 
+
+    compute_resource = compute_resource[server_index]
+    comp = compute_resource * comp_allocation_final
+
+
     # offloading_time = torch.div(tasks, rate+extre) * (args.tasksize_cof/args.band_width)
     offloading_time = torch.div(tasks, rate+extre)
 
-    comp = torch.mul(compute_resource, comp_allocation)
     # compute_time = torch.div(tasks, comp+extre) * (args.tasksize_cof*args.cons_factor/args.comp_cof)
     compute_time = torch.div(tasks, comp+extre)
 
-    time_loss = compute_time + offloading_time
-    time_loss_user = time_loss.sum(1)
 
-    return time_loss_user.mean()
+    time_loss = offloading_time + compute_time
+    assert torch.isnan(time_loss).sum()==0
 
+    time_loss_list = torch.zeros((len(time_loss), user_index[-1]+1), device=args.device)
+    time_loss_list.scatter_(1, user_index.unsqueeze(1), time_loss.unsqueeze(1))
+    time_loss_list = time_loss_list.sum(0)
+
+    return time_loss_list.mean()
 
 
 
@@ -129,10 +180,10 @@ def gnnCritic_train(model, train_layouts, batch_size):
         model.train()
         sches_list = []
         for idx, batch in enumerate(train_layouts):
-            u2s_index = batch['user', 'u2s', 'server'].edge_index
-
-            task_sche, power_sche, comp_sche, sches, critic_value = model(batch.x_dict, batch.edge_index_dict, batch.edge_attr_dict)
-            total_time = compute_loss(task_sche, power_sche, comp_sche, batch['server'].x[:, 2], batch['user', 'u2s', 'server'].path_loss, batch['user'].x[:, 2], u2s_index)
+            task_allocation, power_allocation, comp_allocation, sches, critic_value = model(batch.x_dict, batch.edge_index_dict, batch.edge_attr_dict)
+            user_index = batch['user', 'u2s', 'server'].edge_index[0]
+            server_index = batch['user', 'u2s', 'server'].edge_index[1]
+            total_time = compute_loss(task_allocation, power_allocation, comp_allocation, batch['server'].x[:, 0], batch['user', 'u2s', 'server'].path_loss, batch['user'].x[:, 0], user_index, server_index)
             
             sches_list.append(sches)
 
@@ -179,29 +230,41 @@ def NN_critic_train(model, train_layouts, batch_size):
         model.train()
         sches_list = []
         for idx, batch in enumerate(train_layouts):
+            
             u2s_index = batch['user', 'u2s', 'server'].edge_index
-            u2s_path_loss = batch['user', 'u2s', 'server'].edge_attr.squeeze()
-            user_tasksize = batch['user'].x[:, 2]
-            server_comp_resource = batch['server'].x[:, 2]
-            task_sche, power_sche, comp_sche, sches, critic_value = model(u2s_path_loss, user_tasksize, server_comp_resource, u2s_index)
+            user_index = u2s_index[0]
+            server_index = u2s_index[1]
+            u2s_path_loss = batch['user', 'u2s', 'server'].path_loss.squeeze().reshape((batch_size, -1))
+            u2s_path_loss_feat = batch['user', 'u2s', 'server'].edge_attr.squeeze().reshape((batch_size, -1))
+            user_tasksize = batch['user'].x[:, 0].reshape((batch_size, -1))
+            server_comp_resource = batch['server'].x[:, 0].reshape((batch_size, -1))
+
+            task_sche, power_sche, comp_sche, sches, critic_value = model(u2s_path_loss_feat, user_tasksize, server_comp_resource, u2s_index)
+            total_time = compute_loss_nn(task_sche, power_sche, comp_sche, user_tasksize, server_comp_resource, u2s_path_loss, user_index, server_index)
+            
+            # u2s_index = batch['user', 'u2s', 'server'].edge_index
+            # u2s_path_loss = batch['user', 'u2s', 'server'].edge_attr.squeeze()
+            # user_tasksize = batch['user'].x[:, 0]
+            # server_comp_resource = batch['server'].x[:, 0]
+            # task_sche, power_sche, comp_sche, sches, critic_value = model(u2s_path_loss, user_tasksize, server_comp_resource, u2s_index)
             # task_sche, power_sche, comp_sche, sches, critic_value = self.nnCritic_model(u2s_path_loss, u2s_index)
-            total_time = compute_loss(task_sche, power_sche, comp_sche, batch['server'].x[:, 2], batch['user', 'u2s', 'server'].path_loss, batch['user'].x[:, 2], u2s_index)
+            # total_time = compute_loss_nn(task_sche, power_sche, comp_sche, batch['server'].x[:, 0], batch['user', 'u2s', 'server'].path_loss, batch['user'].x[:, 0], u2s_index)
             
             sches_list.append(sches)
 
             critic_loss = critic_loss_func(total_time.detach(), critic_value)
             critic_loss.backward()
 
-            if idx % batch_size == 0:
-                critic_optimizer.step()
-                critic_optimizer.zero_grad()
-                
-                sches_list = torch.vstack(sches_list)
-                value_loss = model.critic_mlp(sches_list)
-                sches_list = []
-                value_loss.mean().backward()
-                actor_optimizer.step()
-                actor_optimizer.zero_grad()
+            # if idx % batch_size == 0:
+            critic_optimizer.step()
+            critic_optimizer.zero_grad()
+            
+            sches_list = torch.vstack(sches_list)
+            value_loss = -model.critic_mlp(sches_list)
+            sches_list = []
+            value_loss.mean().backward()
+            actor_optimizer.step()
+            actor_optimizer.zero_grad()
             sum_loss += total_time.item()
             length += 1
         
@@ -371,12 +434,15 @@ def NN_train(model, loader):
         model.train()
         for idx, batch in enumerate(loader):
             u2s_index = batch['user', 'u2s', 'server'].edge_index
-            u2s_path_loss = batch['user', 'u2s', 'server'].edge_attr.squeeze().reshape((batch_size, -1))
+            user_index = u2s_index[0]
+            server_index = u2s_index[1]
+            u2s_path_loss = batch['user', 'u2s', 'server'].path_loss.squeeze().reshape((batch_size, -1))
+            u2s_path_loss_feat = batch['user', 'u2s', 'server'].edge_attr.squeeze().reshape((batch_size, -1))
             user_tasksize = batch['user'].x[:, 0].reshape((batch_size, -1))
             server_comp_resource = batch['server'].x[:, 0].reshape((batch_size, -1))
 
-            task_sche, power_sche, comp_sche = model(u2s_path_loss, user_tasksize, server_comp_resource, u2s_index)
-            loss = compute_loss_nn(server_num, task_sche, power_sche, comp_sche, server_comp_resource, batch['user', 'u2s', 'server'].path_loss, user_tasksize)
+            task_sche, power_sche, comp_sche = model(u2s_path_loss_feat, user_tasksize, server_comp_resource, u2s_index)
+            loss = compute_loss_nn(task_sche, power_sche, comp_sche, user_tasksize, server_comp_resource, u2s_path_loss, user_index, server_index)
             loss.backward()
             # if idx % batch_size == 0:
             optimizer.step()
@@ -428,7 +494,7 @@ if __name__ == '__main__':
     train_loader = DataLoader(train_layouts, batch_size=batch_size, shuffle=False)
     # test_loader = DataLoader(test_layouts, batch_size=args.batch_size, shuffle=False)
 
-    single_task_target, single_power_target, single_comp_target = MMSE_test(train_layouts)
+    # single_task_target, single_power_target, single_comp_target = MMSE_test(train_layouts)
     # batch_task_target, batch_power_target, batch_comp_target = MMSE_test(train_loader)
 
     # gnn_supervised_model = TaskLoad(args.num_layers, args.input_dim, args.hidden_dim, args.max_server_num, args.alpha).to(args.device)
@@ -441,9 +507,9 @@ if __name__ == '__main__':
     
     
     # PcNetCritic_model = PCNetCritic((server_num**2)*3+server_num*4, args.hidden_dim, (server_num**2)*3, args.alpha, args).to(args.device)
-    # HGnnCritic_model = GnnCritic(args.input_dim, args.hidden_dim, (server_num**2)*3, args.alpha).to(args.device)
+    # HGnnCritic_model = GnnCritic(args.num_layers, args.input_dim, args.hidden_dim, (server_num**2)*3, args.alpha).to(args.device)
 
-    # pcNet_critic_losses = NN_critic_train(PcNetCritic_model, train_layouts, batch_size)
+    # pcNet_critic_losses = NN_critic_train(PcNetCritic_model, train_loader, batch_size)
     # HGNN_critic_losses= gnnCritic_train(HGnnCritic_model, train_layouts, batch_size)
 
 
